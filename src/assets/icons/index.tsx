a716dacb46c117585e05589e73f3f037965ebf7f
snzlbo <sainzolboo619@gmail.com>
2022-11-22 23:24:55 +0800

    pckg change

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -5,5 +5,4 @@ export { default as MoreIcon } from "./More";
 export { default as PhoneIcon } from "./Phone";
 export { default as EmailIcon } from "./Email";
 export { default as PrintIcon } from "./Print";
-export { default as Notification } from "./Notification";
 export { default as Dot } from "./Dot";
